9d25fcba2856164a2468eced17c8371060190361
qball <qball@09075e82-0dd4-0310-85a5-a0d7c8717e4f>
2004-02-28 15:06:30 +0000

    Fix for bug 190
    git-svn-id: https://svn.musicpd.org/gmpc/trunk@118 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- src/player.c
+++ src/player.c
@@ -291,7 +291,7 @@ void change_progress_update()
 int progress_seek_stop()
 {
 	msg_pop_popup();
-	if(info.connection == NULL) return TRUE;
+	if(info.connection == NULL || info.status->state != MPD_STATUS_STATE_PLAY) return TRUE;
 	else 
 	{
 		GtkRange *scale = (GtkRange *)glade_xml_get_widget(xml_main_window, "progress_slider");
