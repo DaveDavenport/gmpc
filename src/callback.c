1e9465cf8e841c48f31b344fc035afccefc4141e
qball <qball@09075e82-0dd4-0310-85a5-a0d7c8717e4f>
2004-07-12 20:29:36 +0000

    Fixed make dist, fixed shank bug
    git-svn-id: https://svn.musicpd.org/gmpc/trunk@1857 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- src/callback.c
+++ src/callback.c
@@ -7,6 +7,7 @@
 
 int check_for_errors()
 {
+	if(info.connection == NULL) return TRUE;
 	/* check for an error */
 	if(info.connection->error)
 	{
@@ -27,21 +28,12 @@ int check_for_errors()
 			/*Set some stuff right.  */
 			scroll.exposed = 1;
 			info.song = -1;
-			gtk_widget_set_sensitive(glade_xml_get_widget(xml_main_window, "pm_button"), FALSE);
-/*			if(info.playlist_running)
-			{
-				destroy_playlist(glade_xml_get_widget(xml_playlist_window, "playlist_window"));
-				if(debug)g_print("destroying playlist\n");
-			}
-*/			info.state = -1;
-//			clear_playlist_buffer();
+			info.state = -1;
 
-			/* set update timeout slower.. we dont want it to update every 400 ms */
 			gtk_timeout_remove(update_timeout);
 			update_timeout =  gtk_timeout_add(5000, (GSourceFunc)update_interface, NULL);
 		}
 		else info.conlock = FALSE;
-		g_print("Found error: %s\n", info.connection->errorStr);
 		/* clear all error's so it doesnt annoy me later */
 		if(info.connection) mpd_clearError(info.connection);
 		return TRUE;
