3d8c081087deeace31a5d421fcbf911688afcfca
qball <qball@09075e82-0dd4-0310-85a5-a0d7c8717e4f>
2005-03-01 16:16:14 +0000

    added missing file
    git-svn-id: https://svn.musicpd.org/gmpc/trunk@2999 09075e82-0dd4-0310-85a5-a0d7c8717e4f

---
+++ src/config-defaults.h
@@ -0,0 +1,5 @@
+#define DEFAULT_MARKUP_BROWSER 	"[%name%: &[%artist% - ]%title%]|%name%|[%artist% - ]%title%|%shortfile%|"
+#define DEFAULT_PLAYLIST_MARKUP "[%name%: &[%artist% - ]%title%]|%name%|[%artist% - ]%title%|%shortfile%|"
+#define DEFAULT_PLAYER_MARKUP 	"[%name%: &[%artist% - ]%title%]|%name%|[%artist% - ]%title%|%shortfile%|"
+#define DEFAULT_HIDE_ON_STARTUP 0
+#define DEFAULT_TRAY_ICON_ENABLE 1
