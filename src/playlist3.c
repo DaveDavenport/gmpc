05bea93690035e143b5a7a6996416e80f66a0e90
qball <qball@09075e82-0dd4-0310-85a5-a0d7c8717e4f>
2005-08-06 18:33:56 +0000

    more bug fixes
    git-svn-id: https://svn.musicpd.org/gmpc/trunk@3415 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- src/playlist3.c
+++ src/playlist3.c
@@ -2401,7 +2401,7 @@ void pl2_save_playlist ()
 				{
 					if(mpd_ob_playlist_save(connection, str) == MPD_O_PLAYLIST_EXIST )
 					{
-						gchar *errormsg = g_strdup_printf("<i>Playlist <b>\"%s\"</b> allready exists</i>", str);
+						gchar *errormsg = g_strdup_printf(_("<i>Playlist <b>\"%s\"</b> already exists</i>"), str);
 						gtk_label_set_markup(GTK_LABEL(glade_xml_get_widget(xml, "label_error")), errormsg);
 						gtk_widget_show(glade_xml_get_widget(xml, "hbox5"));
 						run = TRUE;
