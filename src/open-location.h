fac685389113ae40220f38a2480a8ffaf0974b59
qball <qball@09075e82-0dd4-0310-85a5-a0d7c8717e4f>
2005-10-27 08:58:14 +0000

    be gone bad files
    git-svn-id: https://svn.musicpd.org/gmpc/trunk@3569 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- src/open-location.h
+++
@@ -1,8 +0,0 @@
-#ifdef ENABLE_GNOME_VFS
-void ol_create(GtkWidget *wid);
-void ol_destroy();
-void ol_create_url(GtkWidget *wid,char *url);
-void ol_drag_data_recieved(GtkWidget *window, GdkDragContext *context,
-		gint x, gint y, GtkSelectionData *selection_data,
-		guint info, guint time);
-#endif
