ef6c7793a3b8cb7aad499135774b3a98afba4ef1
qball <qball@09075e82-0dd4-0310-85a5-a0d7c8717e4f>
2005-10-29 22:16:51 +0000

    fixed plugin buildscripts.
    git-svn-id: https://svn.musicpd.org/gmpc/trunk@3596 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- plugin/libnotify/make_plugin.sh
+++ plugin/libnotify/make_plugin.sh
@@ -1,4 +1,4 @@
-gcc plugin.c -o plugin_libnotify.so -shared -ldl `pkg-config --libs --cflags gmodule-2.0 libxml-2.0 gtk+-2.0 glib-2.0 libnotify`
+gcc plugin.c -o plugin_libnotify.so -shared -ldl `pkg-config --libs --cflags gmodule-2.0 libxml-2.0 gtk+-2.0 glib-2.0 libnotify "libmpd >= 0.0.9.6"`
 
 cp plugin_libnotify.so ~/.gmpc/plugins/
 
--- plugin/wikipedia/make_plugin.sh
+++ plugin/wikipedia/make_plugin.sh
@@ -1,4 +1,4 @@
-gcc plugin.c -o plugin_wp.so -shared -ldl `pkg-config --libs --cflags gmodule-2.0 libxml-2.0 gtk+-2.0 glib-2.0 firefox-gtkmozembed`
+gcc plugin.c -o plugin_wp.so -shared -ldl `pkg-config --libs --cflags gmodule-2.0 libxml-2.0 gtk+-2.0 glib-2.0 firefox-gtkmozembed "libmpd >= 0.0.9.6"`
 
 cp plugin_wp.so ~/.gmpc/plugins/
 
