3945890116afbf71358830114d2a1ff6de9e5dc0
qball <qball@09075e82-0dd4-0310-85a5-a0d7c8717e4f>
2005-11-29 13:37:19 +0000

    removing the wikipedia plugin
    git-svn-id: https://svn.musicpd.org/gmpc/trunk@3710 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- plugin/wikipedia/make_plugin.sh
+++
@@ -1,5 +0,0 @@
-gcc plugin.c -o plugin_wp.so -shared -ldl `pkg-config --libs --cflags gmodule-2.0 libxml-2.0 gtk+-2.0 glib-2.0 firefox-gtkmozembed "libmpd >= 0.0.9.6"`
-
-cp plugin_wp.so ~/.gmpc/plugins/
-
-
