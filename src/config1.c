383bf9cab01342871db915810b4b53210a558b41
qball <qball@09075e82-0dd4-0310-85a5-a0d7c8717e4f>
2006-01-05 16:08:25 +0000

    New config system, please test
    git-svn-id: https://svn.musicpd.org/gmpc/trunk@3779 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- src/config1.c
+++ src/config1.c
@@ -21,6 +21,131 @@
 #include <stdlib.h>
 #include <string.h>
 #include "config1.h"
+void cfg_remove_node(config_obj *cfg, config_node *node);
+config_node *cfg_add_class(config_obj *cfg, char *class);
+config_node *cfg_new_node();
+void cfg_add_child(config_node *parent, config_node *child);
+	
+void cfg_open_parse_file(config_obj *cfgo, FILE *fp)
+{
+	char buffer[1024];
+	int len = 0;
+	int c;
+	config_node *cur = NULL;
+	config_node *multiple = NULL;
+	while((c = fgetc(fp)) != EOF)
+	{
+		if(c == '[')
+		{
+			len =0;
+			c = fgetc(fp);
+			while(c != ']' && c != EOF && len < 1024){
+				buffer[len] = c;
+				len++;
+				c = fgetc(fp);
+			}
+			buffer[len] = '\0';
+			if(len > 0 && len < 256)
+			{
+				cur = cfg_add_class(cfgo, buffer);
+			}
+			/* seek end of line */
+			while(c != EOF && c != '\n') c = fgetc(fp);
+		}
+		if(cur && c == '{')
+		{
+			len =0;                                    		
+			c = fgetc(fp);
+			while(c != '}' && c != EOF && len < 1024){
+				buffer[len] = c;
+				len++;
+				c = fgetc(fp);
+			}
+			buffer[len] = '\0';
+			if(len > 0 && len < 256)
+			{
+				config_node *child = cfg_new_node();
+				child->type = TYPE_ITEM_MULTIPLE;
+				child->name = g_strndup(buffer, len);
+				child->children = NULL;
+				cfg_add_child(cur, child);
+				multiple = child;
+			}
+			if(len==0)
+			{
+				multiple = NULL;
+			}
+			/* seek end of line */
+			while(c != EOF && c != '\n') c = fgetc(fp);
+		}
+		
+		/* next, ignore commants  and there must be a category*/
+		else if(cur && (c  == '#' || c == '/' || c == '\n')){
+			while(c != EOF && c != '\n') c = fgetc(fp);
+		}
+		else if(cur){
+			config_node *new = NULL;
+			gchar *key = NULL;
+			len = 0;
+			while(c != '=' && c != EOF){
+				buffer[len] = c;
+				len++;
+				c = fgetc(fp);
+			}
+			if(len < 256 && len > 0)
+			{
+				int quote=0;
+	
+				/* write key name */
+				new = cfg_new_node();
+				new->type = TYPE_ITEM;
+				new->name = g_strndup(buffer, len);	
+				/* Get value */
+				len = 0;
+				/* skip spaces */
+				while((c = fgetc(fp)) == ' ');
+				/* we got a quoted string */
+				if(c == '"')
+				{
+					quote= 1;
+					c = fgetc(fp);
+				}
+				do{
+					/* add escaped char */
+					if(c == '\\'){
+						c = fgetc(fp);
+						if(c == 'n')
+						{
+							buffer[len] = '\n';
+							len++;
+						}
+						else
+						{
+							buffer[len] = c;
+							len++;
+						}
+					}
+					/* We have a quoted string, and the closing quote comes */
+					else if(c == '"' && quote) quote = -1;
+					else{
+						buffer[len] = c;
+						len++;
+					}
+					c = fgetc(fp);
+				}while((c != '\n' || quote) && c != EOF && quote >= 0);
+				new->value = g_strndup(buffer, len);
+				if(multiple){
+					cfg_add_child(multiple,new);
+				}else{
+					cfg_add_child(cur, new);
+				}
+			}
+			/* seek end of line */
+			while(c != EOF && c != '\n') c = fgetc(fp);			
+		}
+		else while(c != EOF && c != '\n') c = fgetc(fp);			
+	}
+}
 
 config_obj *cfg_open(gchar *url)
 {
@@ -30,7 +155,7 @@ config_obj *cfg_open(gchar *url)
 	{
 		return NULL;
 	}
-	
+
 	cfgo = g_malloc(sizeof(config_obj));
 	/* check if malloc went ok */
 	if(cfgo == NULL)
@@ -39,19 +164,17 @@ config_obj *cfg_open(gchar *url)
 	}
 
 	cfgo->url = g_strdup(url);
+	cfgo->root = NULL;
 	if(g_file_test(cfgo->url, G_FILE_TEST_EXISTS))
 	{
-		cfgo->xmldoc = xmlParseFile(cfgo->url); 
-		cfgo->root = xmlDocGetRootElement(cfgo->xmldoc);
+		FILE *fp = fopen(cfgo->url, "r");
+		if(fp)
+		{
+			cfg_open_parse_file(cfgo, fp);
+			fclose(fp);
+		}
 	}
-	else
-	{
-		cfgo->xmldoc = xmlNewDoc((xmlChar *)"1.0");
-		cfgo->root = xmlNewDocNode(cfgo->xmldoc, NULL, (xmlChar *)"config",NULL);	
-		xmlDocSetRootElement(cfgo->xmldoc, cfgo->root);
-		/* save it directly */
-		cfg_save(cfgo);
-	}
+
 	return cfgo;
 }
 
@@ -62,18 +185,107 @@ void config_close(config_obj *cfgo)
 	{
 		return;
 	}
-	cfg_save(cfgo);
 	if(cfgo->url != NULL)
 	{
 		g_free(cfgo->url);
 	}
-	if(cfgo->xmldoc != NULL)
-	{
-		xmlFreeDoc(cfgo->xmldoc);
-
-	}
-	xmlCleanupParser();
+	while(cfgo->root)cfg_remove_node(cfgo,cfgo->root);
 	g_free(cfgo);
+}
+config_node *cfg_new_node()
+{
+	config_node *newnode = g_malloc0(sizeof(config_node));
+	newnode->type = TYPE_CATEGORY;
+	newnode->name = NULL; 
+	newnode->next = NULL;
+	newnode->prev = NULL;                                 
+	newnode->parent = NULL;
+	newnode->value = NULL;
+	return newnode;
+}
+config_node *cfg_add_class(config_obj *cfg, char *class)
+{
+	config_node *newnode = cfg_new_node();
+	newnode->type = TYPE_CATEGORY;
+	newnode->name = g_strdup(class);
+	newnode->value = NULL;
+	newnode->children  = NULL;
+	if(cfg->root == NULL)
+	{
+		cfg->root = newnode;		
+	}
+	else
+	{
+		config_node *temp = cfg->root;
+		while(temp->next != NULL) temp = temp->next;
+		temp->next = newnode;
+		newnode->prev = temp;
+	}
+	return newnode;
+}
+void cfg_add_child(config_node *parent, config_node *child)
+{
+	if(parent == NULL || child == NULL) return;
+	if(parent->type == TYPE_ITEM ) return;
+	if(parent->children == NULL)
+	{
+		parent->children = child;
+		child->parent = parent;
+	}
+	else
+	{
+		config_node *temp = parent->children;
+
+		/* get last node */
+		while(temp->next != NULL) temp = temp->next;
+		temp->next = child;
+		child->prev = temp;	
+		child->parent = parent;
+	}
+}
+
+void cfg_save_category(config_obj *cfg, config_node *node, FILE *fp)
+{
+	config_node *temp = NULL;
+	if(node == NULL)return;
+	/* find the first */
+	while(node->prev != NULL) node = node->prev;
+	/* save some stuff */
+	for(temp = node;temp != NULL; temp = temp->next){
+		if(temp->type == TYPE_CATEGORY)
+		{
+			fprintf(fp, "[%s]\n",temp->name);
+			cfg_save_category(cfg,temp->children,fp);
+		}
+		if(temp->type == TYPE_ITEM_MULTIPLE)
+		{
+			fprintf(fp, "{%s}\n",temp->name);
+			cfg_save_category(cfg,temp->children,fp);
+			fprintf(fp, "{}\n");
+		}                                                		
+		else if (temp->type == TYPE_ITEM)
+		{
+			int i= 0;
+			fprintf(fp, "%s=\"", temp->name);
+			for(i=0;i<strlen(temp->value);i++)
+			{
+				if(temp->value[i] == '"'){
+					fputs("\\\"",fp);
+				}
+				else if(temp->value[i] == '\\'){
+					fputs("\\\\",fp);
+				}
+				else if(temp->value[i] == '\n'){
+					fputs("\\n",fp);
+				}
+				else{
+					fputc(temp->value[i],fp);
+				}
+			}
+			fputs("\"\n",fp);
+		}
+	}
+
 }
 
 void cfg_save(config_obj *cfgo)
@@ -82,50 +294,53 @@ void cfg_save(config_obj *cfgo)
 	{
 		return;
 	}
-	xmlSaveFormatFile(cfgo->url, cfgo->xmldoc,1);
+	if(cfgo->root != NULL)
+	{
+		FILE *fp = fopen(cfgo->url, "w");
+		if(!fp) return;
+		cfg_save_category(cfgo,cfgo->root, fp);	
+		fclose(fp);
+
+	}
+	//	xmlSaveFormatFile(cfgo->url, cfgo->xmldoc,1);
 	return;
 }
 
-xmlNodePtr cfg_get_class(config_obj *cfg, char *class)
+config_node *cfg_get_class(config_obj *cfg, char *class)
 {
-	xmlNodePtr cur = cfg->root->xmlChildrenNode;
-	if(cur == NULL)
+	config_node *node = cfg->root;
+	if(node == NULL) return NULL;
+	/* find the first */
+	while(node->prev != NULL) node = node->prev;
+
+	for(; node!= NULL; node = node->next)
 	{
-		return NULL;
-	}
-	do
-	{
-		if(xmlStrEqual(cur->name, (xmlChar *)class))
+		if(node->type == TYPE_CATEGORY && !strcmp(node->name, class))
 		{
-			return cur;
+			return node;
 		}
-		cur = cur->next;
-	}while (cur != NULL);
+	}
+
 	return NULL;
 }
 
-xmlNodePtr cfg_get_single_value(config_obj *cfg, char *class, char *key)
+config_node *cfg_get_single_value(config_obj *cfg, char *class, char *key)
 {
 	/* take children */
-	xmlNodePtr cur = cfg_get_class(cfg, class);
-	
-	if(cur == NULL)
+	config_node *cur = cfg_get_class(cfg, class);
+
+	if(cur == NULL || cur->children == NULL)
 	{
 		return NULL;
 	}
-	cur = cur->xmlChildrenNode;
-	if(cur == NULL)
-	{
-		return NULL;
-	}
-	do
-	{
-		if(xmlStrEqual(cur->name, (xmlChar *)key))
+	cur = cur->children;
+	for(;cur != NULL; cur = cur->next) {
+		if(!strcmp(cur->name, key))
 		{
 			return cur;
 		}
-		cur = cur->next;
-	}while (cur != NULL);
+	}
+
 	return NULL;                                     	
 }
 
@@ -133,16 +348,18 @@ void cfg_free_string(char *string)
 {
 	if(string != NULL)
 	{
-		xmlFree(string);
 	}
 }
 
 char * cfg_get_single_value_as_string(config_obj *cfg, char *class, char *key)
 {
-	xmlNodePtr cur = cfg_get_single_value(cfg, class,key);
+	config_node *cur = cfg_get_single_value(cfg, class,key);
 	if(cur != NULL)
 	{
-		return (char *)xmlNodeGetContent(cur);
+		if(cur->type == TYPE_ITEM)
+		{
+			return (char *)cur->value;
+		}
 	}
 	return NULL;
 }
@@ -160,23 +377,23 @@ char * cfg_get_single_value_as_string_with_default(config_obj *cfg, char *class,
 
 int cfg_get_single_value_as_int(config_obj *cfg, char *class, char *key)
 {
-	char * temp = cfg_get_single_value_as_string(cfg,class,key);
-	int result = 0;
-	if(temp == NULL)
+	config_node *cur = cfg_get_single_value(cfg, class,key);
+	if(cur != NULL)
 	{
-		return CFG_INT_NOT_DEFINED;
+		if(cur->type == TYPE_ITEM)
+		{
+			return (int)g_ascii_strtoull(cur->value,NULL,0);	
+
+		}
 	}
-	/* make it return an error */
-	result = atoi(temp);
-	xmlFree(temp);
-	return result;
+	return CFG_INT_NOT_DEFINED;
 }
 
 void cfg_set_single_value_as_int(config_obj *cfg, char *class, char *key, int value)
 {
-	char *value1 = g_strdup_printf("%i", value);
-	cfg_set_single_value_as_string(cfg,class,key,value1);
-	g_free(value1);
+	gchar *	temp = g_strdup_printf("%i",value);
+	cfg_set_single_value_as_string(cfg,class,key,temp);
+	g_free(temp);
 }
 int cfg_get_single_value_as_int_with_default(config_obj *cfg, char *class, char *key, int def)
 {
@@ -186,7 +403,6 @@ int cfg_get_single_value_as_int_with_default(config_obj *cfg, char *class, char 
 		cfg_set_single_value_as_int(cfg,class,key,def);
 		retv = cfg_get_single_value_as_int(cfg,class,key);		
 	}
-	/* make it return an error */
 	return retv;
 }
 /* float */
@@ -200,7 +416,6 @@ float cfg_get_single_value_as_float(config_obj *cfg, char *class, char *key)
 	}
 	/* make it return an error */
 	result = g_ascii_strtod(temp,NULL);
-	xmlFree(temp);
 	return result;
 }
 
@@ -210,7 +425,6 @@ void cfg_set_single_value_as_float(config_obj *cfg, char *class, char *key, floa
 	cfg_set_single_value_as_string(cfg,class,key,value1);
 	g_free(value1);
 }
-
 float cfg_get_single_value_as_float_with_default(config_obj *cfg, char *class, char *key, float def)
 {
 	float retv = cfg_get_single_value_as_float(cfg,class,key);
@@ -222,173 +436,197 @@ float cfg_get_single_value_as_float_with_default(config_obj *cfg, char *class, c
 	/* make it return an error */
 	return retv;
 }
-
+void cfg_remove_node(config_obj *cfg, config_node *node)
+{
+	if(node->type != TYPE_ITEM)
+	{
+		printf("Still children in the node, going to delete it recursive\n");
+		while(node->children)
+		{
+			cfg_remove_node(cfg,node->children);
+		}
+	}
+	/*  only child, and I have a parent */
+	if(node->next == NULL && node->prev == NULL && node->parent)
+	{
+		/* remove from list */	
+		if(node->parent->type != TYPE_ITEM)
+		{
+			node->parent->children = NULL;
+		}
+	}
+	/* remove node from linked list */
+	if(node->next != NULL)
+	{
+		if(node->parent && node->parent->children == node)
+		{
+			node->parent->children = node->next;
+		}
+		node->next->prev = node->prev;
+	}
+	if(node->prev != NULL)
+	{
+		if(node->parent && node->parent->children == node)
+		{
+			node->parent->children = node->prev;		
+		}
+		node->prev->next = node->next;
+	}
+	if(node == cfg->root)
+	{
+		if(node->next){
+			cfg->root = node->next;
+		}else if (node->prev){
+			cfg->root = node->prev;
+		}else{
+			cfg->root = NULL;
+		}
+	}	
+	if(node->name) g_free(node->name);
+	if(node->value) g_free(node->value);
+	g_free(node);
+}
 void cfg_del_single_value(config_obj *cfg, char *class, char *key)
 {
-	xmlNodePtr cur = cfg_get_single_value(cfg,class,key);
-	xmlUnlinkNode(cur);
-	xmlFreeNode(cur);
+	config_node *node = cfg_get_single_value(cfg,class,key);
+	if(node == NULL) return;
+	cfg_remove_node(cfg,node);
 	cfg_save(cfg);
 }
 
 void cfg_set_single_value_as_string(config_obj *cfg, char *class, char *key, char *value)
 {
-	xmlNodePtr cur = cfg_get_single_value(cfg,class,key);
-	gchar *string;
-	if(value == NULL)
-	{
-		g_print("ERROR: trying to set value NULL\n");
-		return;
-	}
-	if(cur != NULL)
-	{
-		cfg_del_single_value(cfg,class,key);
-		cur = NULL;
-	}
-	cur = cfg_get_class(cfg,class);
-	if(cur == NULL)
-	{
-		cur = xmlNewChild(cfg->root, NULL,(xmlChar *)class, NULL);
-	}
-	string = g_markup_escape_text(value, g_utf8_strlen(value, -1));
-	if(string != NULL)
-	{
-		xmlNewChild(cur,NULL, (xmlChar *)key, (xmlChar *)string);
-		g_free(string);
-	}
+	config_node *newnode = cfg_get_single_value(cfg,class,key);
+	if(newnode == NULL)
+	{	
+		config_node *node = cfg_get_class(cfg, class);
+		if(node == NULL)
+		{
+			node = cfg_add_class(cfg, class);	
+			if(node == NULL) return;
+		}
+		newnode = cfg_new_node();
+		newnode->name = g_strdup(key);
+		cfg_add_child(node,newnode);
+
+	}	
+	newnode->type = TYPE_ITEM;
+	if(newnode->value) g_free(newnode->value);
+	newnode->value = g_strdup(value);	
 	cfg_save(cfg);
 }
 
 /* multiple values */
 
 
-xmlNodePtr cfg_get_multiple_value(config_obj *cfg, char *class, char *key, char* id)
+config_node *cfg_get_multiple_value(config_obj *cfg, char *class, char *key, char* id)
 {
-	xmlNodePtr cur = cfg_get_class(cfg, class);
-	if(cur == NULL)
-	{
+	config_node *node = cfg_get_single_value(cfg,class,key);
+	if(node == NULL){
+	 	printf("node %s not found\n", key);      
 		return NULL;
 	}
-	cur = cur->xmlChildrenNode;
-	if(cur == NULL)
-	{
+	if(node->type != TYPE_ITEM_MULTIPLE){
+		printf("no multiple\n");
+	       	return NULL;
+	}
+	if(node->children == NULL){
+	 	printf("empty multiple\n");      
 		return NULL;
 	}
-	do
-	{
-		if(xmlStrEqual(cur->name, (xmlChar *)key) && xmlStrEqual(xmlGetProp(cur, (const xmlChar *)"id"),(xmlChar *) id))
+	/* first*/
+	node = node->children;
+	while(node->prev != NULL) node = node->prev;
+	/* get item */
+	for(; node != NULL;node = node->next) {
+		printf("%s\n", node->name);
+		if(!strcmp(node->name, id)) 
 		{
-			return cur;
+			return node;
 		}
-		cur = cur->next;
-	}while (cur != NULL);
-	return NULL;                                     	
+	}
+	return NULL;
 }
 
 void cfg_del_multiple_value(config_obj *cfg, char *class, char *key,char *id)
 {
-	xmlNodePtr cur = cfg_get_multiple_value(cfg, class,key,id);
+	config_node *cur = cfg_get_multiple_value(cfg, class,key,id);
 	if(cur != NULL)
 	{
-		xmlUnlinkNode(cur);
-		xmlFreeNode(cur);  				
+		cfg_remove_node(cfg,cur);
 		cfg_save(cfg);
 	}
 }
 
 void cfg_set_multiple_value_as_string(config_obj *cfg, char *class, char *key, char *id, char *value)
 {
-	xmlNodePtr test,cur;
-	char *string;
-	cur= cfg_get_multiple_value(cfg,class,key,id);
-	if(value == NULL)
-	{
-		return;
-	}
+	config_node *cur = cfg_get_multiple_value(cfg, class,key,id);
+	printf("found node: %p\n",cur);
 	if(cur != NULL)
 	{
-		xmlUnlinkNode(cur);
-		xmlFreeNode(cur);  		
+		if(cur->value) g_free(cur->value);
+		cur->value = g_strdup(value);
 	}
-
-	string = g_markup_escape_text(value, g_utf8_strlen(value, -1));
-	cur = cfg_get_class(cfg,class);
-	if(cur == NULL)
-	{
-		cur = xmlNewChild(cfg->root, NULL, (xmlChar *)class, NULL);
+	else {
+		config_node *node = cfg_get_single_value(cfg,class,key);
+		if(node == NULL) {
+			node = cfg_get_class(cfg,class);
+			if(node == NULL)
+			{
+				node = cfg_add_class(cfg,class);
+			}
+			cur = cfg_new_node();
+			cur->name = g_strdup(key);
+			cur->type = TYPE_ITEM_MULTIPLE;
+			cfg_add_child(node,cur);
+			node = cur;
+		}
+		if(node->type != TYPE_ITEM_MULTIPLE) return;
+		cur = cfg_new_node();
+		cur->type = TYPE_ITEM;
+		cur->name = g_strdup(id);
+		cur->value = g_strdup(value);
+		cfg_add_child(node,cur);
+		cfg_save(cfg);
 	}
-	test = xmlNewChild(cur,NULL, (xmlChar *)key, (xmlChar *)string);
-	g_free(string);
-	string = g_markup_escape_text(id, g_utf8_strlen(id, -1));
-	g_print("id: %s\n",string);
-	xmlSetProp(test, (xmlChar *)"id",(xmlChar *) string); 
-	g_free(string);
-	cfg_save(cfg);
 }
 
 conf_mult_obj * cfg_get_multiple_as_string(config_obj *cfg, char *class, char *key)
 {
-	xmlNodePtr cur;
-	conf_mult_obj *list = NULL, *first= NULL; 
-	cur = cfg_get_class(cfg, class);
-	if(cur == NULL)
+	config_node *cur = cfg_get_single_value(cfg, class,key);
+	if(cur && cur->type == TYPE_ITEM_MULTIPLE && cur->children != NULL)
 	{
-		return NULL;
+		conf_mult_obj *list = NULL;
+		cur = cur->children;	
+		/* get first */
+		while(cur->prev != NULL) cur = cur->prev;
+		do {
+			conf_mult_obj *temp= g_malloc0(sizeof(conf_mult_obj));
+			temp->value = cur->value;
+			temp->key = cur->name;
+			temp->next = list;
+			if(temp->next) temp->next->prev = temp;
+			list = temp;
+			cur = cur->next;
+		}while(cur != NULL);		
+		return list;
 	}
-	cur = cur->xmlChildrenNode;
-	if(cur == NULL)
-	{
-		return NULL;
-	}
-	do
-	{
-		if(xmlStrEqual(cur->name, (xmlChar *)key))
-		{
-			char *temp;
-			if(list == NULL)
-			{
-				first = list = g_malloc(sizeof(conf_mult_obj));
-				list->next = NULL;
-				list->prev = NULL;
-			}
-			else
-			{
-				list->next = g_malloc(sizeof(conf_mult_obj));
-				list->next->next =  NULL;
-				list->next->prev = list;
-				list = list->next;
-			}
-			temp = (char *)xmlGetProp(cur, (xmlChar *)"id");
-			list->key = g_strdup(temp);
-			xmlFree(temp);
-			temp = (char *)xmlNodeGetContent(cur);
-			list->value = g_strdup(temp);
-			xmlFree(temp);
-		}
-		cur = cur->next;
-	}while (cur != NULL);
-	return first;                                     	
+	return NULL;
 }
-
 void cfg_free_multiple(conf_mult_obj *data)
 {
 	conf_mult_obj *list = data;
+	if(list == NULL) return;
+	/* go to first */
 	while(list->prev != NULL)
 	{
 		list = list->prev;
 	}
 	while(list != NULL)
 	{
-		if(list->key != NULL) g_free(list->key);
-		if(list->value != NULL) g_free(list->value);
-		list = list->next;
-	}
-	list = data;
-	while(list != NULL)
-	{
 		if(list->next != NULL)
 		{
-			g_free(list->prev);
+			if(list->prev)g_free(list->prev);
 			list = list->next;
 		}
 		else{
@@ -397,5 +635,4 @@ void cfg_free_multiple(conf_mult_obj *data)
 			list = NULL;
 		}
 	}
-
 }
--- src/config1.h
+++ src/config1.h
@@ -5,12 +5,33 @@
 #include <libxml/tree.h>
 #define CFG_INT_NOT_DEFINED -65536
 
+typedef enum _ConfigNodeType  {
+	TYPE_CATEGORY,
+	TYPE_ITEM,
+	TYPE_ITEM_MULTIPLE
+} ConfigNodeType;
+
+
+
+typedef struct _config_node {
+	struct _config_node *next;
+	struct _config_node *prev;
+	struct _config_node *parent;
+	gchar *name;
+	ConfigNodeType type;
+	/* union this? */
+	struct _config_node *children; /* TYPE_CATEGORY */
+	gchar *value;/* TYPE_ITEM* */
+
+} config_node;
+
 
 typedef struct config_obj
 {
 	gchar *url;
-	xmlDocPtr xmldoc;
-	xmlNodePtr root;
+	config_node *root;
+//	xmlDocPtr xmldoc;
+//	xmlNodePtr root;
 } config_obj;
 
 
