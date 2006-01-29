df026bb2c98bf532bb00653bc8f012394dd9329b
qball <qball@09075e82-0dd4-0310-85a5-a0d7c8717e4f>
2006-01-29 13:09:31 +0000

    Fix a possible segfault
    git-svn-id: https://svn.musicpd.org/gmpc/trunk@3861 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- src/playlist3.c
+++ src/playlist3.c
@@ -1080,10 +1080,12 @@ void playlist_player_set_song(MpdObj *mi)
 static void playlist_player_update_image_callback(mpd_Song *song)
 {
 	mpd_Song *current = mpd_playlist_get_current_song(connection);
-	if(!strcmp(song->artist, current->artist) &&
-			!strcmp(song->album, current->album))
-	{
-		playlist_player_update_image(connection);
+	if(song != NULL && current  != NULL) {
+		if(!strcmp(song->artist, current->artist) &&
+				!strcmp(song->album, current->album))
+		{
+			playlist_player_update_image(connection);
+		}
 	}
 }
 
