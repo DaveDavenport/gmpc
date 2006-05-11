4d8d245984b8da72bc758255290cf6aad15592cf
qball <qball@09075e82-0dd4-0310-85a5-a0d7c8717e4f>
2006-05-11 10:26:50 +0000

    Fix irritating keybindings, I will miss them.
    git-svn-id: https://svn.musicpd.org/gmpc/trunk@4157 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- src/playlist3.c
+++ src/playlist3.c
@@ -631,19 +631,19 @@ int pl3_window_key_press_event(GtkWidget *mw, GdkEventKey *event)
 	}
 
 	/* default gmpc/xmms/winamp key's*/
-	else if (event->keyval == GDK_z )
+	else if (event->state&GDK_CONTROL_MASK && event->keyval == GDK_z )
 	{
 		prev_song();
 	}
-	else if (event->keyval == GDK_x || event->keyval == GDK_c)
+	else if (event->state&GDK_CONTROL_MASK && (event->keyval == GDK_x || event->keyval == GDK_c))
 	{
 		play_song();
 	}
-	else if (event->keyval == GDK_v)
+	else if (event->state&GDK_CONTROL_MASK && event->keyval == GDK_v)
 	{
 		stop_song();
 	}
-	else if (event->keyval == GDK_b)
+	else if (event->state&GDK_CONTROL_MASK && event->keyval == GDK_b)
 	{
 		next_song();
 	}
