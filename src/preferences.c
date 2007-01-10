df0fcf4d8cdd1eec716fe609d9e1d13307a9608f
qball <qball@09075e82-0dd4-0310-85a5-a0d7c8717e4f>
2007-01-10 21:57:35 +0000

    Change bg-color
    git-svn-id: https://svn.musicpd.org/gmpc/trunk@5234 09075e82-0dd4-0310-85a5-a0d7c8717e4f

--- src/preferences.c
+++ src/preferences.c
@@ -187,10 +187,10 @@ void create_preferences_window()
 	}
 
 	label = glade_xml_get_widget(xml_preferences_window, "plugin_label_box");
-	gtk_widget_modify_bg(label, GTK_STATE_NORMAL, &dialog->style->bg[GTK_STATE_SELECTED]);
-	label = glade_xml_get_widget(xml_preferences_window, "plugin_label");
+	gtk_widget_modify_bg(label, GTK_STATE_NORMAL, &dialog->style->light[GTK_STATE_SELECTED]);
+/*	label = glade_xml_get_widget(xml_preferences_window, "plugin_label");
 	gtk_widget_modify_fg(label, GTK_STATE_NORMAL, &dialog->style->fg[GTK_STATE_SELECTED]);
-	glade_xml_signal_autoconnect(xml_preferences_window);	
+*/	glade_xml_signal_autoconnect(xml_preferences_window);	
 
 }
 
