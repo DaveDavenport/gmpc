684dfcd34054033f205e3eb4db1f7eaac1c20009
Qball Cow <Qball@Sarine.nl>
2008-03-12 20:18:34 +0100

    Also search path for a artist metadata request

--- src/metadata.c
+++ src/metadata.c
@@ -64,6 +64,19 @@ static mpd_Song *rewrite_mpd_song(mpd_Song *tsong, MetaDataType type)
                 mpd_data_free(data2);
             }
         }
+        if(type&(META_ARTIST_ART|META_ARTIST_TXT))
+        {
+            MpdData *data2 = NULL;
+            mpd_database_search_start(connection, TRUE);
+            mpd_database_search_add_constraint(connection, MPD_TAG_ITEM_ARTIST, tsong->artist);
+            data2 = mpd_database_search_commit(connection);
+            if(data2)
+            {
+                edited = data2->song;
+                data2->song = NULL;
+                mpd_data_free(data2);
+            }
+        }
     }
     if(!edited)
         edited = mpd_songDup(tsong);
