1f82ab474eabcf594379b31258d50ff6d5f061f0
Qball Cow <qball@sarine.nl>
2008-11-10 12:45:26 +0100

    Fix copyright years

--- src/edit_markup.c
+++ src/edit_markup.c
@@ -1,5 +1,5 @@
 /*
- *Copyright (C) 2004-2007 Qball Cow <qball@sarine.nl>
+ *Copyright (C) 2004-2008 Qball Cow <qball@sarine.nl>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
