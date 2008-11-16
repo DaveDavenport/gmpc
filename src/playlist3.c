bea3a37d223c85a2ffe83172c3729ba3ff33a14d
Qball Cow <qball@sarine.nl>
2008-11-16 10:22:03 +0100

    Make the button bar a config option

--- src/playlist3.c
+++ src/playlist3.c
@@ -1006,12 +1006,13 @@ void create_playlist3 ()
                 G_TYPE_STRING   /* Num items */
 				);
 	}
-
-    g_signal_connect(G_OBJECT(pl3_tree), "row_inserted", G_CALLBACK(thv_row_inserted_signal), NULL);
-    g_signal_connect(G_OBJECT(pl3_tree), "row_changed", G_CALLBACK(thv_row_changed_signal), NULL);
-    g_signal_connect(G_OBJECT(pl3_tree), "row_deleted", G_CALLBACK(thv_row_deleted_signal), NULL);
-    g_signal_connect(G_OBJECT(pl3_tree), "rows_reordered", G_CALLBACK(thv_row_reordered_signal), NULL);
-
+    if(cfg_get_single_value_as_int_with_default(config, "playlist", "button-heading", FALSE))
+    {
+        g_signal_connect(G_OBJECT(pl3_tree), "row_inserted", G_CALLBACK(thv_row_inserted_signal), NULL);
+        g_signal_connect(G_OBJECT(pl3_tree), "row_changed", G_CALLBACK(thv_row_changed_signal), NULL);
+        g_signal_connect(G_OBJECT(pl3_tree), "row_deleted", G_CALLBACK(thv_row_deleted_signal), NULL);
+        g_signal_connect(G_OBJECT(pl3_tree), "rows_reordered", G_CALLBACK(thv_row_reordered_signal), NULL);
+    }
 
 	tree = glade_xml_get_widget (pl3_xml, "cat_tree");
 
@@ -1533,6 +1534,7 @@ static void playlist_zoom_level_changed()
 	}
 	gtk_widget_show(glade_xml_get_widget(pl3_xml, "vbox5"));
 	gtk_widget_hide(glade_xml_get_widget(pl3_xml, "bread_crumb"));
+    gtk_widget_hide(glade_xml_get_widget(pl3_xml, "box_tab_bar"));
 	gtk_widget_show(glade_xml_get_widget(pl3_xml, "menu_option"));
 	gtk_widget_show(glade_xml_get_widget(pl3_xml, "menu_go"));
 
@@ -1562,7 +1564,10 @@ static void playlist_zoom_level_changed()
 
 		case PLAYLIST_SMALL:
 			gtk_widget_hide(glade_xml_get_widget(pl3_xml, "vbox5"));
-//			gtk_widget_show(glade_xml_get_widget(pl3_xml, "bread_crumb"));
+            if(!cfg_get_single_value_as_int_with_default(config, "playlist", "button-heading", FALSE))
+                gtk_widget_show(glade_xml_get_widget(pl3_xml, "bread_crumb"));
+            else
+                gtk_widget_show(glade_xml_get_widget(pl3_xml, "box_tab_bar"));
 
 			gtk_widget_grab_focus(glade_xml_get_widget(pl3_xml, "pl3_win"));
 		default:
@@ -2271,13 +2276,17 @@ gmpcPlugin playlist_plug = {
 /***
  * Tabbed view hooks 
  */
+/* List keeping track of all the button */
 GList *thv_list = NULL;
+/* Tab button structure */
 typedef struct _TabButton{
     GtkButton *button;
     GtkImage *image;
     GtkLabel *label;
     gint pos;
 }TabButton;
+
+/* if a row in the sidebar changed (f.e. title or icon) update the button accordingly */
 void thv_row_changed_signal ( GtkTreeModel *model, GtkTreePath *path, GtkTreeIter *iter, gpointer data)
 {
     gint *ind = gtk_tree_path_get_indices(path);
@@ -2286,29 +2295,34 @@ void thv_row_changed_signal ( GtkTreeModel *model, GtkTreePath *path, GtkTreeIte
     {
         gchar *title, *image;
         gtk_tree_model_get(model, iter,3, &image, 1, &title, -1); 
+        /* update label */
         if ( title ) {
             gtk_label_set_text(tb->label, title);
             gtk_widget_set_tooltip_text(GTK_WIDGET(tb->label), title);
         }
+        /* update image */
         if ( image ) {
             gtk_image_set_from_icon_name(tb->image, image, GTK_ICON_SIZE_MENU);
         }
-
+        /* free gtk_tree_model_get items */
         if(title)g_free(title);
         if(image)g_free(image);
     }
-    printf("Row Changed\n");
 }
 
 void thv_row_deleted_signal ( GtkTreeModel *model, GtkTreePath *path, gpointer data)
 {
     gint *ind = gtk_tree_path_get_indices(path);
     TabButton *tb = g_list_nth_data(thv_list,ind[0]);
+    /* Remove from the thv_list */
     thv_list = g_list_remove(thv_list, tb);
+    /* Remove from the top bar and also destroy/free the widgets */
     gtk_widget_destroy(GTK_WIDGET(tb->button));
+    /* Free the TabButton structure */
     g_free(tb);
-    printf("Row Removed\n");
 }
+
+/* Sorts TabButton based on positition */
 static int thv_sort_func(gconstpointer a, gconstpointer b)
 {
     TabButton *tb1 = (TabButton *)a;
@@ -2320,26 +2334,31 @@ void thv_row_reordered_signal ( GtkTreeModel *model, GtkTreePath *path, GtkTreeI
     gint *r = arg3;
     int length = gtk_tree_model_iter_n_children(model, NULL);
     int i;
-    GList *iter = NULL;//g_list_first(thv_list);
+    GList *iter = NULL;
+
+    /* Fix the position entries, and remove all the buttons from the header */
     for(i=0; i < length;i++)
     {
         TabButton *tb = g_list_nth_data(thv_list, r[i]);
         tb->pos = i;
         gtk_container_remove(GTK_CONTAINER(glade_xml_get_widget(pl3_xml, "box_tab_bar")), GTK_WIDGET(tb->button));
     }
-    /* build the list in new order */
+    /* Add the buttons to the header in the right order */ 
     thv_list = g_list_sort(thv_list, thv_sort_func);
     for(iter = g_list_first(thv_list);iter; iter = g_list_next(iter))
     {
         TabButton *tb = iter->data;
         gtk_box_pack_start(GTK_BOX(glade_xml_get_widget(pl3_xml, "box_tab_bar")),GTK_WIDGET(tb->button), FALSE, TRUE, 0);   
     }
-    printf("Row reordered\n");
 }
+
+/**
+ * If the button is clicked, trigger an update by calling a select on the sidebar
+ */
 static void thv_button_clicked ( GtkButton *button, TabButton *tb  )
 {
     GtkTreeSelection *selec = gtk_tree_view_get_selection(GTK_TREE_VIEW(glade_xml_get_widget(pl3_xml, "cat_tree")));
-    GtkTreePath *path = gtk_tree_path_new_from_indices(tb->pos/*g_list_index(thv_list, tb)*/, -1);
+    GtkTreePath *path = gtk_tree_path_new_from_indices(tb->pos, -1);
     gtk_tree_selection_select_path(selec, path);
     gtk_tree_path_free(path);
 }
@@ -2352,6 +2371,7 @@ void thv_row_inserted_signal ( GtkTreeModel *model, GtkTreePath *path, GtkTreeIt
     gchar *title, *image;
     gtk_tree_model_get(model, iter,3, &image, 1, &title, -1); 
 
+    /* Create new tabbed-button object */
     tb = g_malloc0(sizeof(*tb));
     tb->button = button;
     
@@ -2374,7 +2394,8 @@ void thv_row_inserted_signal ( GtkTreeModel *model, GtkTreePath *path, GtkTreeIt
     {
         GtkLabel *label = (GtkLabel *)gtk_label_new(title?"":title);
         tb->label = label;
-        if(title){
+        /* Set tooltip on the widget, if there is a title */
+        if ( title ) {
             gtk_widget_set_tooltip_text(GTK_WIDGET(label), title);
         }
         /* Ellipsize the label */
@@ -2390,17 +2411,24 @@ void thv_row_inserted_signal ( GtkTreeModel *model, GtkTreePath *path, GtkTreeIt
     /* Show everything */
     gtk_widget_show_all(GTK_WIDGET(button));
 
-    /* Add it to the above view */
+    /* Add an extra reference to the widget, so when it is removed from the heading
+     * box it does not get free'ed
+     */
     g_object_ref(G_OBJECT(button));
+
+    /* Add it to the heading */
     gtk_box_pack_start(GTK_BOX(glade_xml_get_widget(pl3_xml, "box_tab_bar")),GTK_WIDGET(button), FALSE, TRUE, 0);
 
+    /* Set the position in the TabButton */
     {
         gint *in = gtk_tree_path_get_indices(path);
         tb->pos = in[0];
     }
     /* Add this button to the list */
     thv_list = g_list_insert(thv_list, tb,tb->pos);
-    /* hack to fix numbering */
+    /* hack to fix numbering
+     * TODO: if you insert, only the items after inserting need updating.
+     */
     {
         int i = 0;
         GList *si = g_list_first(thv_list);
@@ -2409,8 +2437,10 @@ void thv_row_inserted_signal ( GtkTreeModel *model, GtkTreePath *path, GtkTreeIt
             i++;
         }
     }
+    /* Move the button in the correct position */
     gtk_box_reorder_child(GTK_BOX(glade_xml_get_widget(pl3_xml, "box_tab_bar")), GTK_WIDGET(button), tb->pos);
 
+    /* free (possible) results from gtk_tree_model_get */
     if(title)g_free(title);
     if(image)g_free(image);
 }
