eff221be6e1bd624f8f7f09bee107eb4c126e746
Qball Cow <qball@sarine.nl>
2009-01-07 12:36:18 +0100

    Hopefully fix the mess when finding plugin data dir

--- src/plugin.c
+++ src/plugin.c
@@ -310,11 +310,23 @@ const gchar *gmpc_plugin_get_data_path(gmpcPlugin *plug)
     return g_strdup(plug->path);
 #else
     gchar *url = NULL;
+    if(strncmp(plug->path, PACKAGE_DATA_PREFIX, strlen(PACKAGE_DATA_PREFIX)) == 0)
+    {
+        url = g_build_path(G_DIR_SEPARATOR_S, PACKAGE_DATA_DIR, "gmpc","plugins",NULL);
+    }else{
+       /* Ok it is a homedir */
+        url = g_strdup(plug->path);
+    }
+
+    printf("found url: %s (%s:%s)\n", url, plug->path, PACKAGE_DATA_PREFIX);
+    return url;
+/*
+    gchar *url = NULL;
     gchar **items = g_strsplit(plug->path, G_DIR_SEPARATOR_S, -1);
     int i = 0;
     int has_lib = 0;
-    /* count number of items, and check for the lib entry */
-    for(i = 0; items && items[i];i++) {
+*/    /* count number of items, and check for the lib entry */
+  /*  for(i = 0; items && items[i];i++) {
         if(strcmp(items[i], "lib")==0 ||strcmp(items[i], "lib64")==0  ) {
             g_free(items[i]);
             items[i] = g_strdup("share");
@@ -326,12 +338,13 @@ const gchar *gmpc_plugin_get_data_path(gmpcPlugin *plug)
         url = g_strdup_printf("%c%s%c", G_DIR_SEPARATOR, temp, G_DIR_SEPARATOR);
         g_free(temp);
     }else {
-        /* Ok it is a homedir */
-        url = g_strdup(plug->path);
+*/        /* Ok it is a homedir */
+  /*      url = g_strdup(plug->path);
     }
     g_strfreev(items);
     debug_printf(DEBUG_INFO, "path: %s\n",url);
     return url;
+    */
 #endif
 }
 
