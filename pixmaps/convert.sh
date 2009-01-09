1ba1a39cd9b9e42c29b4c7d1f18347788f25b3f3
Qball Cow <qball@sarine.nl>
2009-01-09 20:02:53 +0100

    Update icons.

--- pixmaps/convert.sh
+++ pixmaps/convert.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-for a in icons/scalable/status/stylized-*-cover.svg; do
+for a in icons/scalable/status/stylized-*cover.svg icons/scalable/status/*-artist.svg; do
     for b in "48x48" "64x64" "96x96" "128x128"; do
         output=${a/scalable/$b};
-        convert -scale $b "$a" "${output/svg/png}"
+        rsvg-convert -o "${output/svg/png}" "$a"
+        convert -scale $b "${output/svg/png}" "${output/svg/png}"
     done
 done
