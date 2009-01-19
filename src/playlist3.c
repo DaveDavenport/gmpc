7bbe0a601d9aaf5db38f137ce181c5d5a9b53bdf
Qball Cow <qball@sarine.nl>
2009-01-19 17:58:57 +0100

    [ServerInformation] Change menu entry to 'Information' and icon to mpd

--- src/playlist3.c
+++ src/playlist3.c
@@ -2027,9 +2027,9 @@ static void playlist3_fill_server_menu(void)
         menu_item = gtk_separator_menu_item_new();
         gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item);
         /* Server Information */
-        menu_item = gtk_image_menu_item_new_with_label(_("Server Information"));
+        menu_item = gtk_image_menu_item_new_with_label(_("Information"));
         gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(menu_item),
-                gtk_image_new_from_stock(GTK_STOCK_INFO, GTK_ICON_SIZE_MENU));
+                gtk_image_new_from_icon_name("mpd", GTK_ICON_SIZE_MENU));
         g_signal_connect(G_OBJECT(menu_item), "activate", G_CALLBACK(serverinformation_show_popup), NULL);
         gtk_menu_shell_append(GTK_MENU_SHELL(menu), menu_item);
 
