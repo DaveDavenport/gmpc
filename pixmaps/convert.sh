26f1a27d9e46e0b166cae20564e9e1135f03e8cb
Qball Cow <qball@sarine.nl>
2009-01-24 23:03:55 +0100

    Update icons. Fix scaling of rendered icons (I hope)

--- pixmaps/convert.sh
+++ pixmaps/convert.sh
@@ -2,9 +2,19 @@
 
 for a in icons/scalable/status/stylized-*cover.svg icons/scalable/status/*-artist.svg; do
     for b in "48" "64" "96" "128"; do
-        size="$bx$b"
+        size=$b'x'$b
         output=${a/scalable/$size};
-        inkscape -z -D -w $b -h $b -e "${output/svg/png}" "$a"
+        width=`inkscape -W "$a"`
+        height=`inkscape -H "$a"`
+        echo ${height/.*/} -ge ${width/.*/}
+        if [ ${height/.*/} -ge ${width/.*/} ]; then 
+            echo inkscape -z -D -h $b -e "${output/svg/png}" "$a";
+            inkscape -z -D -h $b -e "${output/svg/png}" "$a";
+        else
+            echo inkscape -z -D -w $b -e "${output/svg/png}" "$a";
+            inkscape -z -D -w $b -e "${output/svg/png}" "$a";
+        fi
+#        inkscape -z -D -w $b -h $b -e "${output/svg/png}" "$a"
 #        convert -scale $b "${output/svg/png}" "${output/svg/png}"
     done
 done
