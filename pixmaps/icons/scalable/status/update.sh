0b5d5cf6b166ecb3239d999826700538219671df
Qball Cow <qball@sarine.nl>
2009-02-07 01:56:17 +0100

    Fix update script

--- pixmaps/icons/scalable/status/update.sh
+++ pixmaps/icons/scalable/status/update.sh
@@ -1,7 +1,7 @@
-wget "http://gmpc.chymera.net/logobase/art-n.svg" -O no-artist.svg
-wget "http://gmpc.chymera.net/logobase/art-d.svg" -O fetching-artist.svg
-
-wget "http://gmpc.chymera.net/logobase/cov.svg" -O stylized-cover.svg
-wget "http://gmpc.chymera.net/logobase/cov-d.svg" -O stylized-fetching-cover.svg
-wget "http://gmpc.chymera.net/logobase/cov-n.svg" -O stylized-no-cover.svg
+#!/bin/sh
+wget -O no-artist.svg "http://gmpc.chymera.net/logobase/art-n.svg"
+wget -O fetching-artist.svg "http://gmpc.chymera.net/logobase/art-d.svg"
+wget -O stylized-cover.svg "http://gmpc.chymera.net/logobase/cov.svg" 
+wget -O stylized-fetching-cover.svg "http://gmpc.chymera.net/logobase/cov-d.svg" 
+wget -O stylized-no-cover.svg "http://gmpc.chymera.net/logobase/cov-n.svg" 
 
