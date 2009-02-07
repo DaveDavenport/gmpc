c1ea090e1b8e5807195877c5cb6d8ba234f92b4b
Qball Cow <qball@sarine.nl>
2009-02-07 01:41:16 +0100

    Updates

---
+++ pixmaps/icons/scalable/status/update.sh
@@ -0,0 +1,7 @@
+wget "http://gmpc.chymera.net/logobase/art-n.svg" -O no-artist.svg
+wget "http://gmpc.chymera.net/logobase/art-d.svg" -O fetching-artist.svg
+
+wget "http://gmpc.chymera.net/logobase/cov.svg" -O stylized-cover.svg
+wget "http://gmpc.chymera.net/logobase/cov-d.svg" -O stylized-fetching-cover.svg
+wget "http://gmpc.chymera.net/logobase/cov-n.svg" -O stylized-no-cover.svg
+
