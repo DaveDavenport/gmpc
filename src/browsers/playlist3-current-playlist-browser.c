68a5f775240913442e2f44f49e0337abbb11e04b
Qball Cow <qball@sarine.nl>
2009-04-21 20:38:12 +0200

    [PlayQueue] reduce search timeout to 250ms.

--- src/browsers/playlist3-current-playlist-browser.c
+++ src/browsers/playlist3-current-playlist-browser.c
@@ -288,7 +288,7 @@ static void  mod_fill_entry_changed(GtkWidget *entry, PlayQueuePlugin *self)
 {
     if(self->priv->quick_search_timeout != 0)
         g_source_remove(self->priv->quick_search_timeout);
-    self->priv->quick_search_timeout = g_timeout_add_seconds(1, (GSourceFunc)mod_fill_do_entry_changed, self);
+    self->priv->quick_search_timeout = g_timeout_add(250, (GSourceFunc)mod_fill_do_entry_changed, self);
     gtk_widget_show(entry);
 }
 static void mod_fill_entry_activate(GtkWidget *entry, PlayQueuePlugin *self)
