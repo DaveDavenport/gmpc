c008250d1baedd14fb82c58e48a65b416c9cbc75
Qball Cow <qball@sarine.nl>
2009-05-04 18:46:42 +0200

    Add copyright notice to advanced_search.c/h

--- src/advanced_search.c
+++ src/advanced_search.c
@@ -1,3 +1,21 @@
+/* Gnome Music Player Client (GMPC)
+ * Copyright (C) 2004-2009 Qball Cow <qball@sarine.nl>
+ * Project homepage: http://gmpc.wikia.com/
+
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 #include <config.h>
 #include <gtk/gtk.h>
 #include "main.h"
@@ -75,7 +93,7 @@ MpdData * advanced_search(const gchar *query, int in_playlist)
             if(split)g_strfreev(split);
             i++;
         }
-        else 
+        else
         {
             gchar **split = tokenize_string(text[i]);
             int j;
