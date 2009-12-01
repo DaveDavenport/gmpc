666714ac9162506adf4659fe36b5713b8c1f3bb6
Qball Cow <qball@sarine.nl>
2009-12-01 14:46:45 +0100

    Fix the no image on row 0 bug #2705)

--- src/gmpc-vallgrind.sh
+++ src/gmpc-vallgrind.sh
@@ -2,5 +2,6 @@
 G_SLICE=always-malloc G_DEBUG=gc-friendly valgrind \
     --tool=memcheck --leak-check=full --leak-resolution=high \
     --num-callers=20 \
+    --suppressions=../scripts/gtk.suppression \
     ./gmpc > /dev/null
 
