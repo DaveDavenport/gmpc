513128674a79282158140e56da8a192b7a8ef56a
Qball Cow <qball@sarine.nl>
2010-01-07 01:01:49 +0100

    Fixe valgrind run script.

--- src/gmpc-vallgrind.sh
+++ src/gmpc-vallgrind.sh
@@ -1,5 +1,5 @@
 
-G_SLICE=debug-blocks G_DEBUG=gc-friendly valgrind \
+G_SLICE=always-malloc G_DEBUG=gc-friendly valgrind \
     --tool=memcheck --leak-check=full --leak-resolution=high \
     --num-callers=20 \
     --suppressions=../scripts/nip2.supp\
