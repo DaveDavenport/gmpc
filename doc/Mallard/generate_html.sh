055bff50772ce3e619000146af5a66e5d8a99132
LibertyZero <mail@libertyzero.com>
2011-07-29 21:24:39 +0200

    more documentation

--- doc/Mallard/generate_html.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# yes this might not work on all installs, sorry.
-# calling bash fixes calling gnome-doc-tool on debian.
-
-for a in *.page
-do
-    bash gnome-doc-tool xhtml --css-file="style.css" $a
-done    
