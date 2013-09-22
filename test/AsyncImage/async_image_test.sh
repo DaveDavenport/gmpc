e6b9f1ac8b0123829af2d3badf87253a37fd8f24
QC <qball@gmpclient.org>
2013-09-22 12:18:58 +0200

    Remove obsolete and no longer working tests.

--- test/AsyncImage/async_image_test.sh
+++
@@ -1,8 +0,0 @@
-make 
-
-G_SLICE=always-malloc G_DEBUG=gc-friendly valgrind \
-    --tool=memcheck --leak-check=full --leak-resolution=high \
-    --num-callers=20 --show-reachable=yes \
-    --suppressions=../../scripts/gtk.suppression \
-    ./async_image_test > /dev/null
-
