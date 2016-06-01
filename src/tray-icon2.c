9843853ebf1034ee746d7b0b757db70b37ea5f27
Moritz Bruder <muesli4@gmail.com>
2016-06-01 23:28:21 +0200

    Fixed use of gtk_check_version, which ignores higher major versions.

--- src/tray-icon2.c
+++ src/tray-icon2.c
@@ -312,7 +312,7 @@ static void tray_icon2_init(void)
             g_signal_connect(G_OBJECT(tray_icon2_gsi), "popup-menu", G_CALLBACK(tray_icon2_populate_menu), NULL);
             g_signal_connect(G_OBJECT(tray_icon2_gsi), "activate", G_CALLBACK(tray_icon2_activate), NULL);
             g_signal_connect(G_OBJECT(tray_icon2_gsi), "notify::embedded", G_CALLBACK(tray_icon2_embedded_changed), NULL);
-            if (gtk_check_version(2, 15, 0) == NULL)
+            if (GTK_CHECK_VERSION(2, 15, 0))
             {
                 g_signal_connect(G_OBJECT(tray_icon2_gsi), "button-press-event", G_CALLBACK(tray_icon2_button_press_event), NULL);
                 g_signal_connect(G_OBJECT(tray_icon2_gsi), "scroll-event", G_CALLBACK(tray_icon2_button_scroll_event), NULL);
